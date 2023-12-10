5c542bfc69807ed9f5d9117aa3557630ddc185d4
Nikita Smirnov <fiftikness1507@gmail.com>
2023-12-10 21:45:40 +0300

    Initial commit
    Сделана back-end база для авторизации и регистрации с JWT-токенами

---
+++ client/src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="header">
+      <router-link class="logo" to="/">Kora Praktikum</router-link>
+      <nav class="navbar">
+        <router-link class="list-item" to="/">Главная</router-link>
+        <router-link class="list-item" to="/courses">Курсы</router-link>
+        <router-link class="list-item" to="/about">О проекте</router-link>
+      </nav>
+      <nav class="navbar">
+        <router-link class="list-item" to="/login">Вход</router-link>
+        <router-link class="list-item" to="/registrationForm"
+          >Регистрация</router-link
+        >
+      </nav>
+    </div>
+    <!-- <div class="header">
+      <router-link class="logo" to="/">Kora Praktikum</router-link>
+      <nav class="navbar">
+        <router-link class="list-item" to="/">Главная</router-link>
+        <router-link class="list-item" to="/courses">Курсы</router-link>
+        <router-link class="list-item" to="/about">О проекте</router-link>
+      </nav>
+      <nav class="navbar">
+        <router-link class="list-item" to="/profile">Профиль</router-link>
+      </nav>
+    </div> -->
+  </template>
+  
+  <script>
+  import { appTitle } from "@/_config";
+  export default {
+    name: "Header",
+    data() {
+      return {
+        title: appTitle.title,
+      };
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
